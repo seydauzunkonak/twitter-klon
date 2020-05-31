4a4c5edf170eea18dc65916c5e198e7d4c3790ec
seydauzunkonak <52824376+seydauzunkonak@users.noreply.github.com>
2020-06-01 01:59:37 +0300

    tweetedit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,10 @@
-export * from "./authActions";
-export * from "./tweetActions";
+import { combineReducers } from "redux";
+import AuthReducer from "./authReducer";
+import TweetListReducer from "./tweetListReducer";
+import MyTweetListReducer from "./myTweetListReducer";
+
+export default combineReducers({
+  auth: AuthReducer,
+  tweetList: TweetListReducer,
+  myTweetList: MyTweetListReducer,
+});
